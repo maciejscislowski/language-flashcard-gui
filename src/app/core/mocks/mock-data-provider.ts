b2452835eec5edf03b892e329b92a63ef8c27967
Maciej <maciej@MacBook-Pro-Maciej.local>
2021-03-01 22:21:15 +0100

    initial commit

--- src/app/core/mocks/mock-data-provider.ts
+++ src/app/core/mocks/mock-data-provider.ts
@@ -20,52 +20,6 @@ export class MockDataProvider implements InMemoryDbService {
   }
   // HTTP GET interceptor to support offset and limit query parameters
   get(reqInfo: RequestInfo) {
-    // if (reqInfo.query.has('page') && reqInfo.query.has('size') && reqInfo.collection) {
-    //   const offset = +reqInfo.query.get('page')[0];
-    //   const limit = +reqInfo.query.get('size')[0];
-    //   // Remove offset and limit from query parameters so they're not considered as filtering parameters for the
-    //   // collection later on.
-    //   reqInfo.query.delete('page');
-    //   reqInfo.query.delete('size');
-    //   return reqInfo.utils.createResponse$(() => {
-    //     const collection = reqInfo.collection as Array<{ id: any }>;
-    //     let meta: any;
-    //     let data: any;
-    //     if (reqInfo.id) {
-    //       data = reqInfo.utils.findById(collection, reqInfo.id);
-    //     } else {
-    //       const filteredCollection = MockDataProvider.applyQuery(collection, reqInfo.query);
-    //       const totalCount = filteredCollection.length;
-    //       meta = {totalCount};
-    //       console.log(`offset: ${offset}, limit: ${limit}`);
-    //       data = filteredCollection.slice(offset * limit, offset * limit + limit);
-    //       console.log(`Total count: ${totalCount}, and returned data count: ${data.length}`);
-    //     }
-    //     const response = new Page<SearchResultWrapper>();
-    //     response.page = {
-    //       size: limit,
-    //       totalElements: meta.totalCount,
-    //       totalPages: meta.totalCount / limit,
-    //       number: offset,
-    //       previousNumber: 0
-    //     };
-    //     response._embedded = {
-    //       'allie-catalog': []
-    //     };
-    //     response._embedded['allie-catalog'].push(...data)
-    //     const options: ResponseOptions = data ?
-    //       {
-    //         body: response,
-    //         status: STATUS.OK
-    //       } :
-    //       {
-    //         body: {error: `'${reqInfo.collectionName}' with id='${reqInfo.id}' not found`},
-    //         status: STATUS.NOT_FOUND
-    //       };
-    //     return MockDataProvider.finishOptions(options, reqInfo);
-    //   });
-    // } else {
-
       return undefined;
     // }
   }
